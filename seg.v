99ec30de3863dcf47c6b4b09d341089dff41028f
Himingway <badppg@qq.com>
2015-11-19 23:24:11 +0800

    add

--- seg.v
+++ seg.v
@@ -5,4 +5,18 @@ module seg (
 	
 );
 
+parameter Tx =30;
+parameter Ty = 15;
+
+always @(posedge clk or negedge rst_n) begin : proc_cnt
+	if(~rst_n) begin
+		cnt <= 0;
+	end else begin
+		if (cnt == (Tx+Ty)*10-1)
+			cnt <= 10'd0;
+		else
+			cnt <= 1'd1+cnt;
+	end
+end
+
 endmodule
