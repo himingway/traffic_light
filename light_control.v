5331bd04cfe6592564d581df57c7c9b92efee320
Himingway <badppg@qq.com>
2015-11-20 16:39:54 +0800

    fix

--- light_control.v
+++ light_control.v
@@ -1,8 +1,7 @@
 module light_control (
 	input clk,    // Clock
 	input rst_n,  // Asynchronous reset active low
-	output Gx,Rx,Gy,Ry,
-	output seg_signal
+	output Gx,Rx,Gy,Ry
 );
 
 reg rGx;
@@ -11,7 +10,6 @@ reg rRx;
 reg rRy;
 reg [1:0] state;
 reg [20:0] cnt;
-reg rseg_signal;
 
 parameter Tx =30;
 parameter Ty = 15;
@@ -37,11 +35,10 @@ always @(posedge clk or negedge rst_n) begin : proc_light_control
 	end else begin
 		case (state)
 			2'd0:begin 
-				if(cnt == 0)  rseg_signal <= 1;
+				if(cnt == 0)
 				if(cnt == Tx * 10 - 51) begin
 					state <= 3'd1;
 					rGx <= 1'b0;
-					rseg_signal <= 0;
 				end
 				else
 					rGx <= 1'b1;
@@ -52,26 +49,43 @@ always @(posedge clk or negedge rst_n) begin : proc_light_control
 			2'd1:begin 
 				if(cnt == Tx*10 -1) begin
 					state <= 3'd2;
+					rGx <= 1'b0;
+				end
+				else if(cnt == Tx*10 - 6) begin 
+					rGx <= 1'b1;
 				end
 				else if(cnt == Tx*10 - 11) begin 
 					rGx <= 1'b0;
-				end
+					end
+				else if(cnt == Tx*10 - 16) begin 
+					rGx <= 1'b1;
+					end
 				else if(cnt == Tx*10 - 21) begin 
+					rGx <= 1'b0;
+					end
+				else if(cnt == Tx*10 - 26) begin 
 					rGx <= 1'b1;
 					end
 				else if(cnt == Tx*10 - 31) begin 
 					rGx <= 1'b0;
 					end
-				else if(cnt == Tx*10 - 41) begin 
+				else if(cnt == Tx*10 - 36) begin 
 					rGx <= 1'b1;
+					end
+				else if(cnt == Tx*10 - 41) begin 
+					rGx <= 1'b0;
+					end
+				else if(cnt == Tx*10 - 46) begin 
+					rGx <= 1'b1;
+					end
+				else if(cnt == Tx*10 - 51) begin 
+					rGx <= 1'b0;
 					end
 			end
 			2'd2:begin 
-				if(cnt == Tx * 10)  rseg_signal <= 0;
 				if(cnt == (Tx+Ty)*10-51) begin 
 					state <= 3'd3;
 					rGy <= 1'b0;
-					rseg_signal <= 0;
 				end
 				else begin
 					rRy <=1'b0;
@@ -81,21 +95,38 @@ always @(posedge clk or negedge rst_n) begin : proc_light_control
 			end
 			2'd3:begin 
 				if(cnt == (Tx+Ty)*10-1) begin
-					state <= 3'd0;
-					rseg_signal <= 1;
+					rGy <= 1'b0;
+				end
+				else if(cnt == (Tx+Ty)*10-6) begin 
+					rGy <= 1'b1;
 				end
 				else if(cnt == (Tx+Ty)*10-11) begin 
-					rGy <= 1'b0;
-				end
-				else if(cnt == (Tx+Ty)*10-21) begin 
-					rGy<= 1'b1;
+					rGy<= 1'b0;
 					end
+				else if(cnt == (Tx+Ty)*10-16) begin 
+					rGy <= 1'b1;
+					end
+				else if(cnt == (Tx+Ty)*10-21) begin 
+					rGy <= 1'b0;
+					end		
+				if(cnt == (Tx+Ty)*10-26) begin
+					rGy <= 1'b1;
+				end
 				else if(cnt == (Tx+Ty)*10-31) begin 
 					rGy <= 1'b0;
+				end
+				else if(cnt == (Tx+Ty)*10-36) begin 
+					rGy<= 1'b1;
 					end
 				else if(cnt == (Tx+Ty)*10-41) begin 
+					rGy <= 1'b0;
+					end
+				else if(cnt == (Tx+Ty)*10-46) begin 
 					rGy <= 1'b1;
-					end		
+					end
+				else if(cnt == (Tx+Ty)*10-51) begin 
+					rGy <= 1'b0;
+					end				
 			end
 			default : state <= 2'd0;
 		endcase
@@ -106,5 +137,4 @@ assign Gx = rGx;
 assign Gy = rGy;
 assign Rx = rRx;
 assign Ry = rRy;
-assign seg_signal = rseg_signal;
 endmodule
