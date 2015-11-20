4ecce8a8874f1ee16d01e5725aa4202aa23889ad
Himingway <badppg@qq.com>
2015-11-20 11:44:35 +0800

    add

--- num_generate.v
+++ num_generate.v
@@ -1,34 +1,21 @@
-module seg (
+module num_generate (
 	input clk,    // Clock
 	input rst_n, // Asynchronous reset active low
-	output Numout,
+	output [7:0]Numout,
 	output test
 	);
 
 parameter Tx =30;
 parameter Ty = 15;
 
-reg [20:0]cnt;
-reg [7:0] rNumout;
+reg [7:0] rNumout = Tx-1;
 reg [5:0]cnt1;
 reg clk1;
 reg state;
 
-always @(posedge clk or negedge rst_n) begin : proc_cnt
-	if(~rst_n) begin
-		cnt <= 0;
-	end else begin
-		if (cnt == (Tx+Ty)*10-1)
-			cnt <= 10'd0;
-		else
-		cnt <= 1'd1+cnt;
-	end
-end
-
-
 always @(posedge clk or negedge rst_n) begin : proc_Numour
 	if(~rst_n) begin
-		rNumout <= 0;
+		rNumout <= Tx-1;
 		cnt1 <=0;
 		state <= 1'b0;
 	end else begin
@@ -36,23 +23,23 @@ always @(posedge clk or negedge rst_n) begin : proc_Numour
 			1'b0: begin
 			if(cnt1 == 49)begin 
 				cnt1 <= 0;
-				rNumout <= rNumout + 1'b1;
+				rNumout <= rNumout -1'b1;
 			end else 
 			cnt1 <= 1'b1+cnt1;
-			if(rNumout == Tx-1) begin
+			if(rNumout == 8'b11111111) begin
 				state <= 1'b1; 
-				rNumout <= 0;
+				rNumout <= Ty-1;
 			end
 			end
 			1'b1: begin
 			if(cnt1 == 49)begin 
 				cnt1 <= 0;
-				rNumout <= rNumout ＋ 1'b1;
+				rNumout <= rNumout - 1'b1;
 			end else 
 			cnt1 <= 1'b1+cnt1;
-			if(rNumout == Ty-1) begin 
+			if(rNumout == 8'b11111111) begin 
 				state <= 1'b0;
-				rNumout <= 0;
+				rNumout <= Tx-1;
 			end
 			end
 			default : state <= 1'b0;
